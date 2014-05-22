3ab08a75501e2392bd7243452d55e9901b3253af
Angel Angelov <hextwoa@gmail.com>
2014-05-22 15:43:39 +0300

    added event class

---
+++ event.h
@@ -0,0 +1,15 @@
+# pragma once
+
+# include <iostream>
+# include <string>
+
+using namespace std;
+
+class Event{
+private:
+    double hp,mana;
+    int exp;
+public:
+    Event();
+    Event(string)
+};
